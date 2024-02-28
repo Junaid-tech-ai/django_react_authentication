be59849b093b2006a2db80b8079b1fc1bfe0065c
engrjunaid1994 <engr.junaid001@gmail.com>
2024-02-29 01:28:06 +0500

    update build.sh file

--- django_auth_api/build.sh
+++ django_auth_api/build.sh
@@ -4,10 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
+
+python manage.py createsuperuser --no-input
+
 
 python manage.py collectstatic --no-input
 python manage.py migrate
