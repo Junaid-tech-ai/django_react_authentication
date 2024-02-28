222be30eac9479b84c8f745b44a61b4a9c3e68ae
engrjunaid1994 <engr.junaid001@gmail.com>
2024-02-29 02:14:38 +0500

    update custom management commands

--- django_auth_api/account/management/commands/create_super_user.py
+++ django_auth_api/account/management/commands/create_super_user.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from account.models import User
 from django.core.management.base import BaseCommand
 
 
@@ -6,10 +6,10 @@ class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
+        if not User.objects.filter(email='email').exists():
             User.objects.create_superuser(
                 email='admin@admin.com',
-                username='admin',
+                name='admin',
                 password='admin'
             )
         print('Superuser has been created.')
